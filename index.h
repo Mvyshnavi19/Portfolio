8ff8669d6b382520df70ef71d4c0098f05e955bd
Mvyshnavi19 <110924800+Mvyshnavi19@users.noreply.github.com>
2024-09-02 21:16:00 +0530

    Add Files

---
+++ index.h
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meda VYshnavi</title>
+</head>
+<nav>
+    <a href="education.html">education</a>
+    <a href="skills.html">skills</a>
+    <a href="projects.html">Projects</a>
+    <a href="contact.html">Contact Information</a>
+</nav>
+<body >
+    <div class="Left-Panel" style="width: 500px; background-color: rgb(204, 192, 192);">
+        <img src="C:\Users\HP\Downloads\portfolio\Meda Vyshnavi.jpg" alt="Meda Vyshnavi" style="border-radius: 50%; max-height:500px ; padding-left: 60px;">
+    </div>  
+    <div >
+        <p>Hi I am Meda Vyshnavi.</p>
+        <p>Welcome to my portfolio website! I am a passionate and aspiring software engineer with a strong desire to create innovative solutions and push the boundaries of technology with a deep love for coding and Problem-solving,I am constantly seeking oppurtunities to learn and grow in this ever-evolving field</technology></p>
+     </div> 
+     <h1>About Me</h1>
+     <p>I am currently pursuing my final year bachelor's Degree in Information Technology From SASTRA University.I have built a solid foundation in areas such as c++,SQl,Data Structures and Algorithms.I actively seek oppurtunities</p>
+     <p>I have a strong belief in the value of continous learning and staying adaptable in the ever-evolving world of software enineering.I actively seek out opportunities to expand my knowledge and skills, embracing new technologies and approaches.Feel free to explore my work and get in touch if you have any questions or opportunities to collaborate.</p>
+</body>
+</html>
